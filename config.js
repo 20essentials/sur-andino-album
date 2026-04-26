78681c58078fa60c8dd4a242767cbc60d84a0fd0
DominicDev <s0l0estudi4r@gmail.com>
2026-04-26 23:10:32 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'SUR ANDINO';
+export let nameSongs = [
+	"MIX CUMBIAS", /*1*/
+	"SUR ANDINO", /*2*/
+	"CAMINANDO VOY", /*3*/
+	"SIEMPRE ESTÁS CONMIGO", /*4*/
+	"AMOR PERVERSO", /*5*/
+	"CUMBIA SUR (GUINDA)", /*6*/
+	"UN BESO MÁS", /*7*/
+	"YA NO", /*8*/
+	"HASTA SIEMPRE", /*9*/
+	"DILE", /*10*/
+	"YA FUISTE", /*11*/
+	"QUE TE VAYA BIEN", /*12*/
+	"EL ASESINO", /*13*/
+	"SANTIAGO RICO RICO", /*14*/
+	"EL SURICITO", /*15*/
+	"SHERETEROS", /*16*/
+	"MISKI WARMI", /*17*/
+	"EN TU CUMPLEAÑOS", /*18*/
+	"CUMBIA SUR ", /*19*/
+	"BELLA ILUSION", /*20*/
+]
